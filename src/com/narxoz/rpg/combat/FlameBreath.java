e47e6e515398e3a6ea32f0e574ef3aefbdc6e5b4
Zarina <zarina@gmail.com>
2026-02-20 03:09:31 +0500

    Added FlameBreath ability

--- src/com/narxoz/rpg/combat/FlameBreath.java
+++ src/com/narxoz/rpg/combat/FlameBreath.java
@@ -1,20 +1,27 @@
 package com.narxoz.rpg.combat;
-
 public class FlameBreath implements Ability {
+    private final int damage;
+    private final int burnDuration;
+
+    public FlameBreath(int damage, int burnDuration) {
+        this.damage = damage;
+        this.burnDuration = burnDuration;
+    }
+
     @Override
     public String getName() {
         return "Flame Breath";
     }
     @Override
     public int getDamage() {
-        return 150;
+        return damage;
     }
     @Override
     public String getDescription() {
-        return "Deals area fire damage and burns enemies.";
+        return "Deals AoE fire damage and burns target for " + burnDuration + " seconds.";
     }
     @Override
     public Ability clone() {
-        return new FlameBreath();
+        return new FlameBreath(damage, burnDuration);
     }
 }
