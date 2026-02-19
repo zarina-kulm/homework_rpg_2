9affaefd1449c0e12c39d077514b6e8efb806197
Zarina <zarina@gmail.com>
2026-02-20 02:27:41 +0500

    git add . git add src/main/java/com/narxoz/rpg/combat/Ability.java git commit -m "Added Ability interface" git push added LootTable interface

--- src/com/narxoz/rpg/combat/FlameBreath.java
+++ src/com/narxoz/rpg/combat/FlameBreath.java
@@ -1,4 +1,20 @@
 package com.narxoz.rpg.combat;
 
-public class FlameBreath {
+public class FlameBreath implements Ability {
+    @Override
+    public String getName() {
+        return "Flame Breath";
+    }
+    @Override
+    public int getDamage() {
+        return 150;
+    }
+    @Override
+    public String getDescription() {
+        return "Deals area fire damage and burns enemies.";
+    }
+    @Override
+    public Ability clone() {
+        return new FlameBreath();
+    }
 }
